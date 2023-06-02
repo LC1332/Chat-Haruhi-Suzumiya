b054b595ac1769d4aa068d6cf4d70639c3c98a89
chenxi <to_do_chenxi@163.com>
2023-06-02 19:49:22 +0800

    add text_gradio.ipynb

--- src/text.py
+++ src/text.py
@@ -17,11 +17,13 @@ def download_models():
 
 
 class Text:
-    def __init__(self, text_dir, model, num_steps, path=None):
+    def __init__(self, text_dir, model, num_steps, pkl_path=None, dict_path=None, image_path=None):
         self.text_dir = text_dir
         self.model = model
         self.num_steps = num_steps
-        self.path = path
+        self.pkl_path = pkl_path
+        self.dict_path = dict_path
+        self.image_path = image_path
 
     def get_embedding(self, text):
         tokenizer = AutoTokenizer.from_pretrained("silk-road/luotuo-bert")
@@ -50,18 +52,18 @@ class Text:
                     else:
                         text = ''.join(list(line.strip().split(ch)[1])[1:-1])  # 提取「」内的文本
                     text_embeddings[text] = self.get_embedding(text)
-        if is_save and self.path:
-            with open(self.path, 'wb+') as fw:
+        if is_save and self.pkl_path:
+            with open(self.pkl_path, 'wb+') as fw:
                 pickle.dump(text_embeddings, fw)
 
         return text_embeddings
 
     def load(self):
-        if self.path:
-            with open(self.path, 'rb') as f:
+        if self.pkl_path:
+            with open(self.pkl_path, 'rb') as f:
                 return pickle.load(f)
         else:
-            print("No path")
+            print("No pkl_path")
 
     def get_cosine_similarity(self, texts):
         """
@@ -80,30 +82,29 @@ class Text:
             texts_similarity.append(cosine_similarity(query_embedding, embed, dim=0))
         return texts_similarity
 
-    def text_to_image(self, text, dict_path=None, image_path=None):
+    def text_to_image(self, text):
         """
             给定文本出图片
             计算query 和 texts 的相似度，取最高的作为new_query 查询image
             到text_image_dict 读取图片名
             然后到images里面加载该图片然后返回
         """
-        if dict_path and image_path:
+        if self.dict_path and self.image_path:
             text_image = collections.defaultdict()
-            with open(dict_path, 'r') as f:
+            with open(self.dict_path, 'r') as f:
                 data = f.readlines()
                 for sub_text, image in zip(data[::2], data[1::2]):
                     text_image[sub_text.strip()] = image.strip()
             keys = list(text_image.keys())
             keys.insert(0, text)
-            print(keys)
             query_similarity = self.get_cosine_similarity(keys)
             key_index = query_similarity.index(max(query_similarity))
             text = list(text_image.keys())[key_index]
 
             image = text_image[text] + '.jpg'
-            if image in os.listdir(image_path):
-                res = Image.open(image_path + '/' + image)
-                res.show()
+            if image in os.listdir(self.image_path):
+                res = Image.open(self.image_path + '/' + image)
+                # res.show()
                 return res
             else:
                 print("Image doesn't exist")
@@ -115,22 +116,20 @@ class Text:
         texts = list(pkl.keys())
         texts.insert(0, text)
         texts_similarity = self.get_cosine_similarity(texts)
-        print(texts_similarity)
-        print(max(texts_similarity))
         key_index = texts_similarity.index(max(texts_similarity))
         value = list(pkl.keys())[key_index]
         return value
 
 
 if __name__ == '__main__':
-    path = './pkl/texts.pkl'
+    pkl_path = './pkl/texts.pkl'
+    dict_path = "../characters/haruhi/text_image_dict.txt"
+    image_path = "../characters/haruhi/images"
     model = download_models()
-    text = Text("../characters/haruhi/texts", model=model, num_steps=50, path=path)
+    text = Text("../characters/haruhi/texts", model=model, num_steps=50, pkl_path=pkl_path, dict_path=dict_path, image_path=image_path)
     # text.read_text(is_save=True)
     # data = text.load()
     sub_text = "什么？你在说什么啊？我可不会让你这么轻易地逃脱我的视线。SOS团可是需要你这样的人才的。"
-    dict_path = "../characters/haruhi/text_image_dict.txt"
-    image_path = "../characters/haruhi/images"
     # image = text.text_to_image(sub_text, dict_path=dict_path, image_path=image_path)
     # print(image)
     # print(data)
