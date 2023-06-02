cef7aefc848aa01a7868773a14e70bf75cf47b6a
chenxi <to_do_chenxi@163.com>
2023-06-02 10:08:29 +0800

    add text_embedding by chenxi

--- src/text.py
+++ src/text.py
@@ -16,10 +16,11 @@ def download_models():
 
 
 class Text:
-    def __init__(self, text_dir, model, num_steps):
+    def __init__(self, text_dir, model, num_steps, path=None):
         self.text_dir = text_dir
         self.model = model
         self.num_steps = num_steps
+        self.path = path
 
     def get_embedding(self, text):
         tokenizer = AutoTokenizer.from_pretrained("silk-road/luotuo-bert")
@@ -35,31 +36,36 @@ class Text:
             embeddings = model(**inputs, output_hidden_states=True, return_dict=True, sent_emb=True).pooler_output
         return embeddings[0]
 
-    def read_text(self, is_save=False, save_path=None):
+    def read_text(self, is_save=False):
         """抽取、预存"""
         text_embeddings = collections.defaultdict()
         dirs = os.listdir(self.text_dir)
         for dir in dirs:
             with open(self.text_dir + '/' + dir, 'r') as fr:
                 for line in fr.readlines():
-                    text = line.strip()
+                    ch = '：' if '：' in line else ':'
+                    if '旁白' in line:
+                        text = line.strip().split(ch)[1].strip()
+                    else:
+                        text = ''.join(list(line.strip().split(ch)[1])[1:-1])  # 提取「」内的文本
                     text_embeddings[text] = self.get_embedding(text)
-        if is_save and save_path:
-            with open(save_path, 'wb+') as fw:
+        if is_save and self.path:
+            with open(self.path, 'wb+') as fw:
                 pickle.dump(text_embeddings, fw)
 
         return text_embeddings
 
-    def load(self, load_path=None):
-        if load_path:
-            with open(load_path, 'rb') as f:
+    def load(self):
+        if self.path:
+            with open(self.path, 'rb') as f:
                 return pickle.load(f)
         else:
             print("No path")
 
     def get_cosine_similarity(self, text1, text2):
-        embed1 = self.get_embedding(text1)
-        embed2 = self.get_embedding(text2)
+        pkl = self.load()
+        embed1 = pkl[text1] if text1 in pkl.keys() else self.get_embedding(text1)
+        embed2 = pkl[text2] if text2 in pkl.keys() else self.get_embedding(text2)
         return torch.nn.functional.cosine_similarity(embed1, embed2, dim=0)
 
     def text_to_image(self, text, dict_path=None, image_path=None):
@@ -88,11 +94,16 @@ class Text:
 if __name__ == '__main__':
     path = './pkl/texts.pkl'
     model = download_models()
-    text = Text("../characters/haruhi/texts", model=model, num_steps=50)
-    text.read_text(is_save=True, save_path=path)
-    data = text.load(path)
+    text = Text("../characters/haruhi/texts", model=model, num_steps=50, path=path)
+    text.read_text(is_save=True)
+    data = text.load()
     sub_text = "你们之中要是有外星人  未来人  异世界人或者超能力者的话 就尽管来找我吧！"
     dict_path = "../characters/haruhi/text_image_dict.txt"
     image_path = "../characters/haruhi/images"
     image = text.text_to_image(sub_text, dict_path=dict_path, image_path=image_path)
+    print(image)
     print(data)
+    text1 = "我无意中听到一件事。"
+    text2 = "反正不会是什么重要的事。"
+    res = text.get_cosine_similarity(text1, text2)
+    print(res)
