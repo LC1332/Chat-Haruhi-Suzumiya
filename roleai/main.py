dbfac9a34875653ce45549f802c5ca2408f90fbe
fengyunzaidushi <fengxiaoyang163@163.com>
2023-07-01 08:47:55 +0800

    add roleai and its content 02

--- roleai/main.py
+++ roleai/main.py
@@ -5,18 +5,18 @@ from roleai.audio_classify.classify import AudioClassification
 
 def main():
 
-
-    # video segement by subtitle
+    # 使用前 先修改 config.py对应参数
+    # video segement by subtitle 视频切割出分段音频
     video_segmentor = VideoSegmentation(video_config['video_lis_pth'],
                                         audio_config['audio_out_dir'],
                                         srt_config['subtitle_dir'])
     video_segmentor.process()
 
-    # Audio Feature Extraction
+    # Audio Feature Extraction  音频特征提取
     audio_feature_extractor = AudioFeatureExtraction(audio_config['audio_model_pth'])
     audio_feature_extractor.extract_features(audio_config['audio_out_dir'])
 
-    # audio calssify and predict
+    # audio calssify and predict 根据音频特征 识别台本角色
     audio_classification = AudioClassification(audio_config['audio_roles_dir'],
                                                srt_config['srt_out_dir'],
                                                audio_config['audio_out_dir'])
