bf30dffd8547f85fa4429ad21e8fe42cc0572800
chenxi <to_do_chenxi@163.com>
2023-07-04 10:50:27 +0800

    add device

--- src_reform/utils.py
+++ src_reform/utils.py
@@ -9,7 +9,7 @@ def download_models():
     # Import our models. The package will take care of downloading the models automatically
     model_args = Namespace(do_mlm=None, pooler_type="cls", temp=0.05, mlp_only_train=False,
                             init_embeddings_model=None)
-    model = AutoModel.from_pretrained("silk-road/luotuo-bert", trust_remote_code=True, model_args=model_args)
+    model = AutoModel.from_pretrained("silk-road/luotuo-bert", trust_remote_code=True, model_args=model_args).to(device)
     print("Luotuo-Bert下载完毕")
     return model
 
