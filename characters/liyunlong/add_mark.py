d3d32dd4ebfc3fb6bd5a6d84555f0d11111dc6c7
chenxi <to_do_chenxi@163.com>
2023-07-04 16:36:22 +0800

    update liyunlong

--- characters/liyunlong/add_mark.py
+++ characters/liyunlong/add_mark.py
@@ -3,7 +3,7 @@ import os
 
 def add_mark(folder):
     for file in os.listdir(folder):
-        with open(os.path.join(folder, file), 'r', encoding='utf-8') as f1, open('texts/'+file, 'w+', encoding='utf-8') as f2:
+        with open(os.path.join(folder, file), 'r', encoding='utf-8') as f1, open('texts2/'+file, 'w+', encoding='utf-8') as f2:
             for line in f1.readlines():
                 print(line)
                 line = line.replace('：', ':「')
@@ -16,3 +16,6 @@ def add_mark(folder):
                 f2.write('\n')
 
 add_mark('texts')
+# for i in range(1, 15):
+#     with open(f'texts/{i}.txt', 'w+') as f:
+#         print(1)
