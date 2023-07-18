cfabd91530b33075c4ceec86dd6c7a38485648aa
J1shen <133644840+J1shen@users.noreply.github.com>
2023-07-18 20:19:26 +0800

    Update dialogue2embedding.py

--- kyon_generator/dialogue2embedding.py
+++ kyon_generator/dialogue2embedding.py
@@ -1,6 +1,5 @@
 '''
 FILE_NAME: dialogue2embedding.py
-First Edition
 Edited by 睡觉鱼, Jul 18 2023
 '''
 
@@ -33,15 +32,28 @@ def dialogue2embedding(dialogues,tokenizer,model):
     return dialogues
 
 if __name__ == '__main__':
-    DATA_PATH = 'dialogue.json'
+    # 解析命令行参数
+    parser = argparse.ArgumentParser(description='Process embedding data for Chat Haruhi Suzumiya project')
+    parser.add_argument('-dialogue_path', required=True, help='Dialogue path')
+    parser.add_argument('-output', default='output.json', help='Output file path')
+    parser.add_argument('-other_names', nargs='*', default=[], help='Other role names (default: None)')
+    args = parser.parse_args()
+
+    # 处理参数
+    DATA_PATH = args.dialogue_path
+    output_file = args.output
+    other_names = args.other_names
+    
+    #导入模型
     luotuo_tokenizer = AutoTokenizer.from_pretrained("silk-road/luotuo-bert")
     model_args = Namespace(do_mlm=None, pooler_type="cls", temp=0.05, mlp_only_train=False, init_embeddings_model=None)
     luotuo_model = AutoModel.from_pretrained("silk-road/luotuo-bert", trust_remote_code=True, model_args=model_args)
 
-    with open ('dialogue.json') as f:
+    with open (DATA_PATH) as f:
         dialogues = json.load(f)
 
     dialogue_embeddings = dialogue2embedding(dialogues,luotuo_tokenizer,luotuo_model)
-    with open("myfile.json", "w") as o:
+    
+    with open(output_file, "w") as o:
         json.dump(dialogue_embeddings,o)
     
