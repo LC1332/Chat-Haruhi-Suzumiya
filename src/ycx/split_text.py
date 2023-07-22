90d2a77bd77b9f29f862975388d4d51d6ea664f4
chenxi <to_do_chenxi@163.com>
2023-07-22 12:30:54 +0800

    add yuqian

--- src/ycx/split_text.py
+++ src/ycx/split_text.py
@@ -1,10 +1,13 @@
 import os
 
 
-output_folder = 'liyunlong_file_output'
-for file in os.listdir('liyunlong_file'):
-    with open(os.path.join('liyunlong_file', file), encoding='utf-8') as f:
-        data = f.read()
-        for i, dialogue in enumerate(data.split('--')):
-            with open(os.path.join(output_folder, f"{file[:-4]}_{i}.txt"), 'w+', encoding='utf-8') as fw:
-                fw.write(dialogue.strip())
+def split_text(input_folder, output_folder):
+    for file in os.listdir(input_folder):
+        with open(os.path.join(input_folder, file), encoding='utf-8') as f:
+            data = f.read()
+            for i, dialogue in enumerate(data.split('\n\n')):
+                with open(os.path.join(output_folder, f"{file[:-4]}_{i}.txt"), 'w+', encoding='utf-8') as fw:
+                    fw.write(dialogue.strip())
+
+
+split_text('yuqian', 'yuqian_output')
