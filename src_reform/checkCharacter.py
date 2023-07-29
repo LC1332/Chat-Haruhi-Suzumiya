cbcd6b90b1c1f6d7ed4a4a915e003726e08187ed
chenxi <to_do_chenxi@163.com>
2023-07-29 22:53:02 +0800

    add characters

--- src_reform/checkCharacter.py
+++ src_reform/checkCharacter.py
@@ -1,23 +1,42 @@
 import os
 
+
+
+
+
 def checkCharacter(configuration):
     pass
-    if os.path.isdir(configuration['character_folder']):
+    character_folder = configuration['character_folder']
+    if os.path.isdir(character_folder):
         print("已找到角色文件夹")
     else:
-        print("未找到角色文件夹")
-        return False, 0, "未找到角色文件夹"
-    elements = os.listdir(configuration['character_folder'])
+        os.makedirs(character_folder)
+        print("已创建角色文件夹")
+        # return False, 0, "未找到角色文件夹"
+    elements = os.listdir(character_folder)
     if "system_prompt.txt" not in elements:
         # print("未找到系统提示词，请添加")
         return False, 1, "未找到系统提示词, 请添加"
     # print(os.listdir(configuration['character_folder']))
+    if "texts" not in elements:
+        os.makedirs(configuration["texts_folder"])
+        print("texts文件夹创建成功")
     if "jsonl" not in elements:
-        return False, 2, "未找到jsonl文件, 请添加"
-    else:
-        jsonl_path = os.path.join(configuration["character_folder"], "jsonl")
-        # print(jsonl_path)
-        jsonl_elements = os.listdir(jsonl_path)
-        # print(jsonl_elements)
-        
+        os.makedirs(configuration["jsonl_folder"])
+        print("jsonl文件夹创建成功")
+        # return False, 2, "未找到jsonl文件, 请添加"
+    # else:
+    #     jsonl_path = os.path.join(character_folder, "jsonl")
+    #     # print(jsonl_path)
+    #     jsonl_elements = os.listdir(jsonl_path)
+    #     # print(jsonl_elements)
+    if 'dialogues' not in elements:
+        os.makedirs(configuration["dialogue_path"])
+        print("dialogues文件夹创建成功")
+        # return False, 3, "未找到dialogues，请添加"
+    if "images" not in elements:
+        os.makedirs(configuration["images_folder"])
+        print("iamges文件夹创建成功")
     return True, -1, "角色检查完毕"
+
+
